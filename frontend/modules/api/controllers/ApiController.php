4dfa61d1224b811c5dbcda5f60b5a363642f8176
iironside <iironside@yandex.com>
2022-04-14 23:34:27 +0300

    commit corse behavior in apiController

--- frontend/modules/api/controllers/ApiController.php
+++ frontend/modules/api/controllers/ApiController.php
@@ -6,7 +6,7 @@ use yii\filters\ContentNegotiator;
 use yii\helpers\ArrayHelper;
 use yii\rest\Controller;
 use yii\web\Response;
-use common\behaviors\GsCors;
+//use common\behaviors\GsCors;
 
 class ApiController extends Controller
 {
@@ -18,19 +18,19 @@ class ApiController extends Controller
                 'formats' => [
                     'application/json' => Response::FORMAT_JSON,
                 ],
-                'corsFilter' => [
-                    'class' => GsCors::class,
-                    'cors' => [
-                        'Origin' => ['*'],
-                        //'Access-Control-Allow-Credentials' => true,
-                        'Access-Control-Allow-Headers' => [
-                            'Content-Type',
-                            'Access-Control-Allow-Headers',
-                            'Authorization',
-                            'X-Requested-With'
-                        ],
-                    ]
-                ],
+//                'corsFilter' => [
+//                    'class' => GsCors::class,
+//                    'cors' => [
+//                        'Origin' => ['*'],
+//                        //'Access-Control-Allow-Credentials' => true,
+//                        'Access-Control-Allow-Headers' => [
+//                            'Content-Type',
+//                            'Access-Control-Allow-Headers',
+//                            'Authorization',
+//                            'X-Requested-With'
+//                        ],
+//                    ]
+//                ],
             ],
         ]);
     }
