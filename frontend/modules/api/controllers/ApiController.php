7db7d1ec76c6fb4084aec2cf94f3eb24b1aa9cc5
iironside <iironside@yandex.com>
2022-04-15 16:41:53 +0300

    added corse filter

--- frontend/modules/api/controllers/ApiController.php
+++ frontend/modules/api/controllers/ApiController.php
@@ -2,36 +2,35 @@
 
 namespace frontend\modules\api\controllers;
 
+use common\behaviors\GsCors;
 use yii\filters\ContentNegotiator;
-use yii\helpers\ArrayHelper;
 use yii\rest\Controller;
 use yii\web\Response;
-//use common\behaviors\GsCors;
 
 class ApiController extends Controller
 {
     public function behaviors(): array
     {
-        return ArrayHelper::merge(parent::behaviors(), [
+        return [
+            'corsFilter' => [
+                'class' => GsCors::class,
+                'cors' => [
+                    'Origin' => ['*'],
+                    //'Access-Control-Allow-Credentials' => true,
+                    'Access-Control-Allow-Headers' => [
+                        'Content-Type',
+                        'Access-Control-Allow-Headers',
+                        'Authorization',
+                        'X-Requested-With'
+                    ],
+                ]
+            ],
             [
-                'class' => ContentNegotiator::class,
+                'class' => ContentNegotiator::className(),
                 'formats' => [
                     'application/json' => Response::FORMAT_JSON,
                 ],
-//                'corsFilter' => [
-//                    'class' => GsCors::class,
-//                    'cors' => [
-//                        'Origin' => ['*'],
-//                        //'Access-Control-Allow-Credentials' => true,
-//                        'Access-Control-Allow-Headers' => [
-//                            'Content-Type',
-//                            'Access-Control-Allow-Headers',
-//                            'Authorization',
-//                            'X-Requested-With'
-//                        ],
-//                    ]
-//                ],
             ],
-        ]);
+        ];
     }
 }
