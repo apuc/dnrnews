eb94eaca522faeef8f0a20eafb63b3fbdd4e0e73
kavalar <apuc06@mail.ru>
2022-09-11 12:23:40 +0300

    news action

--- backend/modules/news/views/news/index.php
+++ backend/modules/news/views/news/index.php
@@ -45,7 +45,6 @@ $this->params['breadcrumbs'][] = $this->title;
             ],
             [
                 'class' => 'yii\grid\ActionColumn',
-                'template' => '{view} {delete}',
             ],
         ],
     ]); ?>
